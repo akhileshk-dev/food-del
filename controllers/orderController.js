adb363b49a511b9d51995e75bd9316738ab90bdc
akhileshk-dev <139101675+akhileshk-dev@users.noreply.github.com>
2024-07-25 23:39:11 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY);
    
 const placeOrder=async(req,res)=>{
     
-    const frontend_url="http://localhost:5173";
+    const frontend_url="https://food-del-frontend-d7dl.onrender.com";
   
 try {
     
